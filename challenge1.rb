d598cd8b60fa85439a227fe13278628733a909c5
manjula <manjulakp@gmail.com>
2015-12-30 03:54:27 +0530

    commit

--- challenge1.rb
+++ challenge1.rb
@@ -14,20 +14,22 @@ Using what we have learned so far: I wonder if anyone could have a crack at work
 =end
 
 def age_in_years(age_in_seconds)
-days_in_a_year = 365
-months_in_a_year = 12
-seconds_in_a_month = 60 * 60 * 24 * 30
-hours_in_one_day = 24
-seconds_in_year = 60 * 60 * 24 * 365
+  days_in_a_year = 365
+  months_in_a_year = 12
+  seconds_in_a_month = 60 * 60 * 24 * 30
+  hours_in_one_day = 24
+  seconds_in_year = 60 * 60 * 24 * 365
 
-age_year = age_in_seconds.to_f/ seconds_in_year
-age_month = (age_in_seconds.to_f % seconds_in_year) / seconds_in_a_month
-return age_year,age_month
+  age_year = age_in_seconds.to_f / seconds_in_year
+  age_month = (age_in_seconds.to_f % seconds_in_year) / seconds_in_a_month
+  #### return array [age_year, age_month]
+  #### using hash 
+  {years: age_year, months: age_month}
 end
 
 ages =[979000000, 2158493738, 246144023, 1270166272, 1025600095]
 
 ages.each do |age|
-years,month = age_in_years(age)
-puts "The age is #{years} and #{month} "
+  years,month = age_in_years(age)
+  puts "The age is %<years>2i and %<months>i." % age_in_years(age)
 end
