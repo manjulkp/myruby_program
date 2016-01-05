5112f122551073eb05a745e728b46b3047945fe0
manjula <manjulakp@gmail.com>
2016-01-05 19:56:17 +0530

    commit

--- challenge3.rb
+++ challenge3.rb
@@ -1,39 +1,48 @@
-	def multiplication_table (number, heading = false, decorate = false)
-		@table = ''
-		puts   heading 
-		column_width(number)
-		decorate(decorate)
-		table(number)
-		decorate(decorate)
-		@table << "\n"
+def multiplication_table (number, heading = false, decorate = false)
+	@table = ''
+	column_width(number)
+	heading(heading)
+	decorate(decorate,heading)
+	table(number)
+	decorate(decorate,heading)
+	@table << "\n"
+end
+
+def column_width(number)
+	if number == 0 || number == 1
+		@decoration_width = 4
+	else
+		@col_width = (number * number).to_s.length
+		@decoration_width = number.to_i  * @col_width + number.to_i
 	end
+end
 
+def decorate(boolean,heading)
+  if boolean 
+  if	@decoration_width > heading.length
+	@table << '=' * @decoration_width + "\n" 
+  else
+  @table << '=' *	heading.length  +  "\n"
+	end
+end
+end
 
-	def column_width(number)
-		if number == 0 || number == 1
-			@decoration_width = 2
-		else
-			@col_width = (number * number).to_s.length
-			@decoration_width = number.to_i  * @col_width + number.to_i
+def table(num)
+	if num == 0 || num == 1
+		@table << " #{num} \n"
+	else
+		for i in 1..num
+			@table << " %#{num.to_s.length}d " %i    
+			(i+i).step(i * num, i) { |i| @table << "%#{@col_width}d " % i }
+			@table << "\n"
 		end
-	end
+	end 
+end
 
-	def decorate(boolean)
-		@table << '=' * @decoration_width + "\n" if boolean  
-	end
+def heading(heading)
+	@table << ' ' * ((@decoration_width / 2) - heading.length / 2) if @decoration_width > heading.length
+	@table << heading + "\n"
 
-	def table(num)
-		if num == 0 || num == 1
-			@table << " #{num} \n"
-		else
-			for i in 1..num
-				@table << " %#{num.to_s.length}d " %i    
-				(i+i).step(i * num, i) { |i| @table << "%#{@col_width}d " % i }
-				@table << "\n"
-			end
-		end 
-	end
+end
 
-
-
-puts multiplication_table(9, 'multiplicaton table', true)
+puts multiplication_table(0, 'multiplication', true)
